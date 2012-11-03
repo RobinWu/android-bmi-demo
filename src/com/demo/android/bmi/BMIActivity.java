62f6d1a280d2c9ac271c83d86f5b101d217d5d20
RobinWu <robin.wu.cn@gmail.com>
2012-11-03 22:26:54 +0800

    refactory findViews & setListeners

--- src/com/demo/android/bmi/BMIActivity.java
+++ src/com/demo/android/bmi/BMIActivity.java
@@ -17,30 +17,39 @@ public class BMIActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        Button button = (Button)findViewById(R.id.submit);
-        button.setOnClickListener(calcBMI);
+        findViews();
+        setListeners();
     }
     
+    private void setListeners() {
+        buttoncalc.setOnClickListener(calcBMI);
+	}
+
+	private Button buttoncalc;
     private EditText fieldheight;
     private EditText fieldweight;
     private TextView result;
     private TextView suggest;
+    
+    private void findViews() {
+        buttoncalc = (Button)findViewById(R.id.submit);
+		fieldheight = (EditText)findViewById(R.id.height);
+		fieldweight = (EditText)findViewById(R.id.weight);
+		result = (TextView)findViewById(R.id.result);
+		suggest = (TextView)findViewById(R.id.suggest);
+    };
     
     private OnClickListener calcBMI = new OnClickListener() {
 
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			DecimalFormat nf = new DecimalFormat("0.00");
-			fieldheight = (EditText)findViewById(R.id.height);
-			fieldweight = (EditText)findViewById(R.id.weight);
 			double height = Double.parseDouble(fieldheight.getText().toString())/100;
 			double weight = Double.parseDouble(fieldweight.getText().toString());
 			double BMI = weight / (height * height);
 			
-			result = (TextView)findViewById(R.id.result);
 			result.setText("Your result" + nf.format(BMI));
 			
-			suggest = (TextView)findViewById(R.id.suggest);
 			if(BMI > 25) {
 				suggest.setText(R.string.advice_heavy);
 			} else if (BMI < 20) {
